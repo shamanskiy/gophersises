eb633f1541f246846a2b9012dfd6f016aa232d15
Shamanskiy <shamanskiy.alexander@gmail.com>
2022-04-10 08:59:28 +0100

    first version of link parser works

--- link-parser/linkparser.go
+++ link-parser/linkparser.go
@@ -1,1 +1,32 @@
 package linkparser
+
+import (
+	"bytes"
+
+	"golang.org/x/net/html"
+)
+
+func ExtractLinks(htmlDoc []byte) (map[string]string, error) {
+	doc, err := html.Parse(bytes.NewReader(htmlDoc))
+	if err != nil {
+		return nil, err
+	}
+
+	links := map[string]string{}
+	parsingFunc(doc, links)
+
+	return links, nil
+}
+
+func parsingFunc(node *html.Node, links map[string]string) {
+	if node.Type == html.ElementNode && node.Data == "a" {
+		link := node.Attr[0].Val
+		linkText := node.FirstChild.Data
+		links[link] = linkText
+		return
+	}
+
+	for child := node.FirstChild; child != nil; child = child.NextSibling {
+		parsingFunc(child, links)
+	}
+}
--- link-parser/linkparser_test.go
+++ link-parser/linkparser_test.go
@@ -1,1 +1,64 @@
 package linkparser
+
+import "testing"
+
+func TestExtractLinks_SingleLink(t *testing.T) {
+	html := `<html>
+	<body>
+	  <h1>Hello!</h1>
+	  <a href="/other-page">A link to another page</a>
+	</body>
+	</html>
+	links := ExtractLinks(html)`
+
+	got, err := ExtractLinks([]byte(html))
+	want := map[string]string{
+		"/other-page": "A link to another page",
+	}
+
+	if err != nil {
+		t.Errorf("Failed with error: %s\n", err)
+	}
+	if !linkMapsEqual(got, want) {
+		t.Errorf("Wrong links! Got %v, want %v\n", got, want)
+	}
+}
+
+func TestExtractLinks_TwoLinks(t *testing.T) {
+	html := `<html>
+	<body>
+	  <h1>Hello!</h1>
+	  <a href="/other-page">A link to another page</a>
+	  <a href="/another-link">Click me!</a>
+	</body>
+	</html>
+	links := ExtractLinks(html)`
+
+	got, err := ExtractLinks([]byte(html))
+	want := map[string]string{
+		"/other-page":   "A link to another page",
+		"/another-link": "Click me!",
+	}
+
+	if err != nil {
+		t.Errorf("Failed with error: %s\n", err)
+	}
+	if !linkMapsEqual(got, want) {
+		t.Errorf("Wrong links! Got %v, want %v\n", got, want)
+	}
+}
+
+func linkMapsEqual(got, want map[string]string) bool {
+	if len(got) != len(want) {
+		return false
+	}
+
+	for key, got_value := range got {
+		want_value, ok := want[key]
+		if !ok || want_value != got_value {
+			return false
+		}
+	}
+
+	return true
+}
